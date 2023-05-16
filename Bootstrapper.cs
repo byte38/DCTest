0ec3f46d824c6d23cba3e90a61683f502fed4d03
byte38 <byte38@outlook.com>
2023-05-16 04:07:14 -0400

    Add DI

--- Bootstrapper.cs
+++ Bootstrapper.cs
@@ -1,15 +1,31 @@
 ﻿using Caliburn.Micro;
 using DCTest.ViewModels;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Windows;
 
 namespace DCTest {
     public class Bootstrapper : BootstrapperBase {
+        public static IHost ApplicationHost { get; private set; }
+
         public Bootstrapper () {
+            ApplicationHost = Host.CreateDefaultBuilder ()
+                .ConfigureServices ((hostContext, services) => {
+
+                })
+                .Build ();
+
             Initialize ();
         }
 
         protected override async void OnStartup (object sender, StartupEventArgs e) {
+            await ApplicationHost.StartAsync ();
+
             await DisplayRootViewForAsync<MainViewModel> ();
+        }
+
+        protected override async void OnExit (object sender, EventArgs e) {
+            await ApplicationHost.StopAsync ();
         }
     }
 }
